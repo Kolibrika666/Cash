d79633f4f759287d67339aaad7ea1511fe1688c5
Kolibrika666 <kolibrika666@gmail.com>
2024-02-24 21:27:09 +0700

    init: react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/Layout";
+import Balance from "./pages/balance/App";
+import Income from "./pages/Income/App";
+import Expenses from "./pages/expenses/App";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="balance" element={<Balance />} />
+          <Route path="income" element={<Income />} />
+          <Route path="expenses" element={<Expenses />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
